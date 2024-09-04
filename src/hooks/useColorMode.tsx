025b0cf938fde94d14431931bef89676aa2f6b77
Julian Holguin <93228068+jholguinx@users.noreply.github.com>
2024-09-04 17:33:02 -0500

    fixcolorfunction

--- src/hooks/useColorMode.tsx
+++ src/hooks/useColorMode.tsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 
 const useColorMode = () => {
   // State to keep track of the color mode
-  const [colorMode, setColorMode] = useState("light");
+  const [colorMode, setColorMode] = useState<"light" | "dark">("light");
 
   useEffect(() => {
     // Function to handle color mode based on system preference
-    const handleColorModeChange = (e) => {
+    const handleColorModeChange = (e: MediaQueryListEvent) => {
       const newColorMode = e.matches ? "dark" : "light";
       setColorMode(newColorMode);
       const className = "dark";
@@ -18,7 +18,15 @@ const useColorMode = () => {
 
     // Check the system's color mode preference and set the initial color mode
     const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    handleColorModeChange(mediaQuery); // Set initial mode based on system preference
+
+    // Set initial mode based on system preference
+    const initialColorMode = mediaQuery.matches ? "dark" : "light";
+    setColorMode(initialColorMode);
+    const className = "dark";
+    const bodyClass = window.document.body.classList;
+    initialColorMode === "dark"
+      ? bodyClass.add(className)
+      : bodyClass.remove(className);
 
     // Add listener for changes in system color mode preference
     mediaQuery.addEventListener("change", handleColorModeChange);
@@ -28,7 +36,7 @@ const useColorMode = () => {
   }, []);
 
   // Return the color mode and setter (if you need to control it manually)
-  return [colorMode, setColorMode];
+  return [colorMode, setColorMode] as const;
 };
 
 export default useColorMode;
