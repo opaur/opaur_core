0e86c251776b5686706026268045cd5df4a28e47
Julian Holguin <93228068+jholguinx@users.noreply.github.com>
2024-09-04 18:24:47 -0500

    minorfix

--- src/components/Headerc/index.jsx
+++ src/components/Headerc/index.jsx
@@ -6,14 +6,17 @@ import DropdownUser from "../Header/DropdownUser";
 const HeaderComp = () => {
   return (
     <header className="p-4">
-      <nav className="container mx-auto flex items-center justify-between max-w-screen-lg">
+      <nav className="container mx-auto flex max-w-screen-lg items-center justify-between">
         <div className="text-lg font-bold text-black dark:text-white">
           <span className="text-xl">OpAur</span>
         </div>
-       
+
         <div className="flex items-center gap-3 2xsm:gap-7">
           <ul className="flex items-center gap-2 2xsm:gap-4">
-            <DarkModeSwitcher/>
+            <div style={{ display: "none" }}>
+              <DarkModeSwitcher />
+            </div>
+
             {/* Notification Menu Area */}
             <DropdownNotification />
             {/* Chat Notification Area */}
@@ -23,16 +26,15 @@ const HeaderComp = () => {
           <DropdownUser />
           <a
             href="/private"
-            className="inline-flex items-center justify-center rounded-md bg-primary text-center font-medium text-white hover:bg-opacity-90 lg:px-6 xl:px-8 px-4 py-2 text-sm "
+            className="inline-flex items-center justify-center rounded-md bg-primary px-4 py-2 text-center text-sm font-medium text-white hover:bg-opacity-90 lg:px-6 xl:px-8 "
           >
             Get Started
             <span className="ml-1 text-lg">&#8594;</span>
           </a>
-    
         </div>
       </nav>
     </header>
   );
-}
+};
 
 export default HeaderComp;
