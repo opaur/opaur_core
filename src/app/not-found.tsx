d6cec307f77838b04dc503698fcbba3f396598ce
Julian Holguin <93228068+jholguinx@users.noreply.github.com>
2024-09-04 22:37:46 -0500

    lamonda

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div style={{ textAlign: "center", padding: "50px" }}>
       <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for doesn't exist.</p>
+      <p>Aca no hay una mondaa</p>
       <Link href="/">
         Go back to Home
       </Link>
