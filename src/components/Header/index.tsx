e6f84d877b31f0fcb9166fe328f96b0b12ff8d7f
Juan Pablo Herrera Rodriguez <39092027+JuanPabloH@users.noreply.github.com>
2024-09-05 23:15:40 -0500

    dashboard-manejo de sesión

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,11 +4,11 @@ import DropdownMessage from "./DropdownMessage";
 import DropdownNotification from "./DropdownNotification";
 import DropdownUser from "./DropdownUser";
 import Image from "next/image";
-
-const Header = (props: {
-  sidebarOpen: string | boolean | undefined;
-  setSidebarOpen: (arg0: boolean) => void;
-}) => {
+import {
+  type User,
+  createClientComponentClient,
+} from "@supabase/auth-helpers-nextjs";
+const Header = (props: {  sidebarOpen: string | boolean | undefined;  setSidebarOpen: (arg0: boolean) => void;}, {user}:{user: User|null}) => {
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
@@ -121,7 +121,7 @@ const Header = (props: {
           </ul>
 
           {/* <!-- User Area --> */}
-          
+          <DropdownUser user={user}/>
           {/* <!-- User Area --> */}
         </div>
       </div>
