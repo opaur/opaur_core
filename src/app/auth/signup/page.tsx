01af415fc589a139297a902a982b096a45b6a829
Juan Pablo Herrera Rodriguez <39092027+JuanPabloH@users.noreply.github.com>
2024-09-12 20:19:20 -0500

    Validacion de sesión en SignUp

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,13 +1,20 @@
 import { Metadata } from "next";
 import SignUpClient from "./page.client";
-
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation';
 export const metadata: Metadata = {
   title:
     "Opaur - Build Your Brand Aura",
   description: "Opaur - Build Your Brand Aura",
 };
 
-const SignUp: React.FC = () => {
+const SignUp= async() => {
+  const supabase= createServerComponentClient({cookies})
+  const { data: { user },} = await supabase.auth.getUser();
+  if(user){
+    redirect('/dashboard')
+  }
   return (    
       <SignUpClient />    
   );
