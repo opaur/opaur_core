6f837f1106601733c13815919edd33c4c2941cb8
Julian Holguin <93228068+jholguinx@users.noreply.github.com>
2024-10-01 19:15:24 -0500

    contact6

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 import HeaderComp from "@/components/Headerc";
 import Footer from "@/components/Footer/index";
-import Image from "next/image";
 import Link from "next/link";
 import useColorMode from "@/hooks/useColorMode"; // Color mode hook
 import { useState } from "react"; // Import useState to handle selected topic
 import { type User } from "@supabase/auth-helpers-nextjs";
 
 const ContactUs = ({ user }: { user: User | null }) => {
-  const colorMode = useColorMode(); // Initialize color mode
   const [selectedTopic, setSelectedTopic] = useState<string | null>(null); // State for selected topic
 
   const topics = [
