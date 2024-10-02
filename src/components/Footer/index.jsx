0fc08273bdbc3f061dd1b8b9741dbfb47d096598
Julian Holguin <93228068+jholguinx@users.noreply.github.com>
2024-10-01 19:22:26 -0500

    fix

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,91 +2,6 @@ const currentYear = new Date().getFullYear();
 import Link from "next/link";
 
 export default function Footer() {
-<<<<<<< HEAD
-  return (
-    <footer className="p-4">
-      <nav className="container mx-auto flex max-w-screen-lg items-center justify-between">
-        <div className="text-lg font-bold">
-          <span className="text-sm text-slate-400 dark:text-white">
-            {currentYear} Opaur. All rights reserved.
-          </span>
-        </div>
-        <div className="flex space-x-4">
-          <a
-            href="https://www.linkedin.com/company/opaur/"
-            target="_blank"
-            className="px-3 py-2 text-sm transition-colors duration-300 hover:text-blue-500"
-          >
-            <svg
-              class="text-red-300 h-5 w-5"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              {" "}
-              <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />{" "}
-              <rect x="2" y="9" width="4" height="12" />{" "}
-              <circle cx="4" cy="4" r="2" />
-            </svg>
-          </a>
-          <a
-            href="https://www.instagram.com/opaur.ai?igsh=MXVpcTc5dThxZ3FtZg%3D%3D&utm_source=qr"
-            target="_blank"
-            className="px-3 py-2 text-sm transition-colors duration-300 hover:text-blue-500"
-          >
-            <svg
-              class="text-red-300 h-5 w-5"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              {" "}
-              <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />{" "}
-              <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />{" "}
-              <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
-            </svg>
-          </a>
-          <a
-            href="/"
-            className="px-3 py-2 text-sm transition-colors duration-300 hover:text-blue-500"
-          >
-            <svg
-              class="text-red-300 h-5 w-5"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
-              <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
-            </svg>
-            </a>
-            <a
-            href="/"
-            className="px-3 py-2 text-sm transition-colors duration-300 hover:text-blue-500"
-          >          
-          <Link href="/contact" passHref>
-            <span >
-              Contact Us
-            </span>
-          </Link>
-          </a>
-        </div>
-      </nav>
-    </footer>
-  );
-=======
     return (
         
         <footer className="p-4">
@@ -120,5 +35,4 @@ export default function Footer() {
             </nav>
         </footer>
     );
->>>>>>> b0ce9d296d3b3b0fee422b24a5940bea79f059be
 }
