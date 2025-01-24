7c66b45fdeb74e44e40e79483155425df726556e
JuanPabloH <juanpherrerarodriguez@gmail.com>
2025-01-24 10:04:04 -0500

    test api countries 2

--- src/app/api/countries/route.js
+++ src/app/api/countries/route.js
@@ -7,7 +7,7 @@ export async function GET(request) {
     });
   }
 
-  const { data, error } = await supabase.from('countries').select('*');
+  const { data, error } = await supabase.from('countrie').select('*');
 
   if (error) {
     return new Response(JSON.stringify({ error: error.message }), {
