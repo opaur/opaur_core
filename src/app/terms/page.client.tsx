83f2980413186b5562ee2ae57d35b3883eda743a
Julian Holguin <93228068+jholguinx@users.noreply.github.com>
2025-02-23 23:15:48 -0500

    morepages5

--- src/app/terms/page.client.tsx
+++ src/app/terms/page.client.tsx
@@ -198,7 +198,7 @@ The Service is provided to You “AS IS” and “AS AVAILABLE” and with all f
         
           <h3 className="mb-4 text-xl font-bold text-black dark:text-white">Changes to These Terms and Conditions</h3>
           <p className="mb-6 text-gray-600 dark:text-gray-300">
-            We reserve the right, at Our sole discretion, to modify or replace these Terms at any time. If a revision is material We will make reasonable efforts to provide at least 30 days' notice prior to any new terms taking effect. What constitutes a material change will be determined at Our sole discretion. By continuing to access or use Our Service after those revisions become effective, You agree to be bound by the revised terms. If You do not agree to the new terms, in whole or in part, please stop using the website and the Service.
+            We reserve the right, at Our sole discretion, to modify or replace these Terms at any time. If a revision is material We will make reasonable efforts to provide at least 30 days notice prior to any new terms taking effect. What constitutes a material change will be determined at Our sole discretion. By continuing to access or use Our Service after those revisions become effective, You agree to be bound by the revised terms. If You do not agree to the new terms, in whole or in part, please stop using the website and the Service.
           </p>
 
           <h3 className="mb-4 text-xl font-bold text-black dark:text-white">Contact Us</h3>
